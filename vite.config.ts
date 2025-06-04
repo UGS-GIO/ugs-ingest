59d99ce4acd4b102a839ec2d7e9296cf277f3fcf
Jay <jayhill@utah.gov>
2025-06-04 10:07:47 -0600

    chore: tailwindcss installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin' // Import the plugin
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin' 
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
-    TanStackRouterVite(), // Add the TanStack Router plugin here
+    TanStackRouterVite(),
+    tailwindcss(),
   ],
 })
