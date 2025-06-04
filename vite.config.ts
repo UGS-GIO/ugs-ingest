82722ad47bb71a6c2920daacc301a428bfbd038c
Jay <jayhill@utah.gov>
2025-06-04 13:32:39 -0600

    chore: shadcn installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin' 
@@ -10,4 +11,9 @@ export default defineConfig({
     TanStackRouterVite(),
     tailwindcss(),
   ],
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
